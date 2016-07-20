d18d6ccdb4863f3f935a3b3c0b347206dea840bb
Michał Zieliński <michal@zielinscy.org.pl>
2016-07-20 10:05:43 +0200

    subprocess: checkOutput

--- subprocess.nim
+++ subprocess.nim
@@ -1,5 +1,5 @@
 # Easily run commands.
-import osproc, strutils, sequtils, future
+import osproc, strutils, sequtils, future, streams
 
 type CalledProcessError* = object of IOError
   errorCode: int
@@ -17,3 +17,16 @@ proc checkCall*(args: openarray[string], echo=false) =
     let exc = newException(CalledProcessError, "called process returned status code $1" % [$ret])
     exc.errorCode = ret
     raise exc
+
+proc checkOutput*(args: seq[string]): string =
+  let process = startProcess(command=args[0], args=args[1..^1], options={poUsePath})
+  let data = process.outputStream.readAll()
+  let ret = process.waitForExit
+
+  if ret != 0:
+    let exc = newException(CalledProcessError, "called process returned status code $1" % [$ret])
+    exc.errorCode = ret
+    raise exc
+
+  process.close
+  return data
